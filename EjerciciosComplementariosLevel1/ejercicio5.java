89d9b573f5eb298f653aefb65d40117b06c2e0db
Ballest-fran <88737673+Ballest-fran@users.noreply.github.com>
2021-10-12 18:53:45 -0300

    v1.02

--- EjerciciosComplementariosLevel1/ejercicio5.java
+++ EjerciciosComplementariosLevel1/ejercicio5.java
@@ -2,11 +2,38 @@ package EjerciciosComplementariosLevel1;
 import java.util.*;
 
 public class ejercicio5{
+    static int multiPorRecursividad(int a,int b){
+        if (a>0){
+            return b+multiPorRecursividad(a-1, b);
+        }else{
+            return 0;
+        }
+    }
+
+    static int multip (int a,int b){
+        int r=0;
+        boolean ban=false;
+        if (a<0){
+            a=a*(-1);
+            ban=true;
+        }
+        for(int i=0;i<a;i++){
+            r=r+b;
+        }
+        if (ban){
+            return r*(-1);
+        }
+        return r;
+    }
 
     public static void main(String[] args){
+
         Scanner scan = new Scanner(System.in);
-        String nomb = scan.nextLine();
+        System.out.println("ingrese dos numeros para multiplicar: ");
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        System.out.println("Resultado ---> "+num1+" * "+num2+" = "+multip(num1, num2));
+        System.out.println("Resultado, funcion recursiva ---> "+multiPorRecursividad(num1, num2));
         scan.close();
-        System.out.println("HOLA "+nomb);
         }
     }
---
+++ EjerciciosComplementariosLevel1/ejercicio8.java
@@ -0,0 +1,12 @@
+package EjerciciosComplementariosLevel1;
+import java.util.*;
+
+public class ejercicio1{
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String nomb = scan.nextLine();
+        scan.close();
+        System.out.println("HOLA "+nomb);
+        }
+    }
