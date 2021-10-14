3a218679e9254bb2ef9c23d82dd8d1e09585feef
Ballest-fran <88737673+Ballest-fran@users.noreply.github.com>
2021-10-13 22:58:11 -0300

    v1.03

--- EjerciciosComplementariosLevel1/ejercicio6.java
+++ EjerciciosComplementariosLevel1/ejercicio6.java
@@ -1,12 +1,38 @@
 package EjerciciosComplementariosLevel1;
 import java.util.*;
 
-public class ejercicio1{
+public class ejercicio6{
+
+    static float exponenciar(int a,int b){
+        if (b>0){
+            return a*exponenciar(a, b-1);
+        }else{
+            return 1;
+        }
+    }
+
+    static float exponenciarNegat(float a,float b){
+        if (b<0){
+            return (1/a)*exponenciarNegat(a, b+1);
+        }else{
+            return 1;
+        }
+    }
 
     public static void main(String[] args){
+
         Scanner scan = new Scanner(System.in);
-        String nomb = scan.nextLine();
+        System.out.println("ingrese la base: ");
+        int num1 = scan.nextInt();
+        System.out.println("ingrese el exponente: ");
+        int num2 = scan.nextInt();
+        if (num2<0){
+            System.out.println(num1+" elevado "+num2+" = "+exponenciarNegat(num1, num2));
+        }else{
+            System.out.println(num1+" elevado "+num2+" = "+exponenciar(num1, num2));
+        }
+        System.out.println();
+        
         scan.close();
-        System.out.println("HOLA "+nomb);
         }
     }
